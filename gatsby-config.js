e51ac44a5a9c1805b45ca99e62423d98d779e04b
Guilherme Gomes <guiga.gomes10@gmail.com>
2019-12-18 01:54:32 -0300

    Ajustado a cor do tema

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#242424`,
+        theme_color: `#242424`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
