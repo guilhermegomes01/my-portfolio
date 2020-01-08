61604b9cfec4aef352f30ac58ee6ad17c638278f
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-08 02:35:51 -0300

    Criado os itens do portfolio

---
+++ src/components/Portfolio/content.js
@@ -0,0 +1,26 @@
+const sites = [
+    {
+        title: "Veneza Water Park",
+        type: "E-Commerce",
+        name: "veneza"
+    }, 
+    {
+        title: "CCA Advogados",
+        type: "Institucional",
+        name: "cca"
+    },
+    {
+        title: "Melhor Comunicação",
+        type: "Institucional",
+        name: "melhorcom"
+    },
+    {
+        title: "Novo Nascer",
+        type: "Institucional",
+        name: "novoNascer"
+    } 
+]
+
+
+
+export default sites
