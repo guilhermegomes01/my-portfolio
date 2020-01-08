32adb426b542ccdad60f2a4c5c0dfed7eeba55ed
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-08 02:36:05 -0300

    Ajustado o css do portfolio

---
+++ src/components/Portfolio/styled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+
+export const PortfolioWrapper = styled.section``
+
+export const PortfolioFilter = styled.ul``
+
+export const PortfolioFilterItem = styled.li``
+
+export const PortfolioList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`
+export const PortfolioItems = styled.div``
+
+export const PortfolioTitle = styled.h3`
+    font-size: 22px;
+`
+
+export const PortfolioType = styled.span``
+
+export const ImagensWrapper = styled(Img)`
+  border-radius: 7px;
+  width: 280px;
+  height: 80px;
+`
