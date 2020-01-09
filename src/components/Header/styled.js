4e328b48f1c702e4af760fd65b26d132f7718d01
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-09 02:30:03 -0300

    Criado o header

---
+++ src/components/Header/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const HeaderWrapper = styled.section`
+    height: 70px;
+    width: 100%;
+    background: #242424;
+    position: fixed;
+    z-index: 1;
+`
