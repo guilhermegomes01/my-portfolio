7db62203df62d3ec549103967907d91d4920e488
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-09 02:30:55 -0300

    Ajustado o layout do site

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -2,6 +2,9 @@ import styled from 'styled-components'
 
 export const LayoutWrapper = styled.section`
   display: flex;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding-top: 70px;
 `
 
 export const LayoutMain = styled.section`
