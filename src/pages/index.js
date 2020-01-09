5aeb081c393d8c4c642d4482d4710134455808cd
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-09 02:32:19 -0300

    Adicionado o Header na pagina principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,17 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import Portfolio from '../components/Portfolio'
+import Portfolio from "../components/Portfolio"
+import Header from "../components/Header"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Portfolio />
-  </Layout>
+  <>
+    <Header />
+    <Layout>
+      <SEO title="Home" />
+      <Portfolio />
+    </Layout>
+  </>
 )
 
 export default IndexPage
