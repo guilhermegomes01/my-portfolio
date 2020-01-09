a08489ca400e5fadc754f6d34001e3005f6c8fdd
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-09 02:33:10 -0300

    Adicionado código para filtrar o portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
+import Icon from "../../images/svg-icons/surface1.svg"
 import * as S from "./styled"
 import sites from "./content"
+import links from "../MenuLinks/content"
 
 export const squareImage = graphql`
   fragment squareImage on File {
@@ -15,6 +17,10 @@ export const squareImage = graphql`
 `
 
 const Portfolio = () => {
+  const [institucional, setInstitucional] = useState(true)
+  const [ecommerce, setEcommerce] = useState(true)
+
+  /* Imagens */
   const { veneza, cca, melhorcom, novoNascer } = useStaticQuery(graphql`
     query {
       veneza: file(relativePath: { eq: "portfolio/venezawaterpark.png" }) {
@@ -36,20 +42,56 @@ const Portfolio = () => {
 
   return (
     <S.PortfolioWrapper>
-      <h2 className="page-title">Portfolio</h2>
+      <S.PortfolioTitle>
+        <Icon className="icon-title" />
+        <h2 className="page-title">{links[0].label}</h2>
+      </S.PortfolioTitle>
       <S.PortfolioFilter>
-        <S.PortfolioFilterItem>Todos</S.PortfolioFilterItem>
-        <S.PortfolioFilterItem>Website</S.PortfolioFilterItem>
-        <S.PortfolioFilterItem>Institucional</S.PortfolioFilterItem>
+        <S.PortfolioFilterItem
+          className={institucional && ecommerce && "actived"}
+          onClick={() => {
+            setEcommerce(true)
+            setInstitucional(true)
+          }}
+        >
+          Todos
+        </S.PortfolioFilterItem>
+        <S.PortfolioFilterItem
+          className={institucional && !ecommerce && "actived"}
+          onClick={() => {
+            setEcommerce(false)
+            setInstitucional(true)
+          }}
+        >
+          Institucional
+        </S.PortfolioFilterItem>
+        <S.PortfolioFilterItem
+          className={!institucional & ecommerce && "actived"}
+          onClick={() => {
+            setInstitucional(false)
+            setEcommerce(true)
+          }}
+        >
+          E-Commerce
+        </S.PortfolioFilterItem>
       </S.PortfolioFilter>
       <S.PortfolioList>
         {sites.map((site, index) => {
+          const siteInstitucional = site.type === "Institucional"
+          const siteEcommerce = site.type === "E-Commerce"
           return (
-            <S.PortfolioItems key={index}>
-              <S.PortfolioTitle>{site.title}</S.PortfolioTitle>
-              <S.PortfolioType>{site.type}</S.PortfolioType>
-              <S.ImagensWrapper fluid={imgs[site.name].childImageSharp.fluid} />
-            </S.PortfolioItems>
+            <>
+              {((institucional && siteInstitucional) ||
+                (ecommerce && siteEcommerce)) && (
+                <S.PortfolioItems key={index}>
+                  <S.PortfolioItemTitle>{site.title}</S.PortfolioItemTitle>
+                  <S.PortfolioType>{site.type}</S.PortfolioType>
+                  <S.ImagensWrapper
+                    fluid={imgs[site.name].childImageSharp.fluid}
+                  />
+                </S.PortfolioItems>
+              )}
+            </>
           )
         })}
       </S.PortfolioList>
