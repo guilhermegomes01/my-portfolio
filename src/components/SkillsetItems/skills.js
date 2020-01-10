b7c411d45459471fe59174ffb67c43985914f4b0
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-10 01:28:14 -0300

    Adicionado as habilidades gerais e tecnicas

---
+++ src/components/SkillsetItems/skills.js
@@ -0,0 +1,8 @@
+const skills = [
+    'Kanban',
+    'Proatividade',
+    'Design Responsivo',
+    'Trabalho em Equipe'
+]
+
+export default skills
---
+++ src/components/SkillsetItems/tech.js
@@ -0,0 +1,13 @@
+const techs = [
+    'HTML5',
+    'CSS3',
+    'SASS',
+    'Javascript',
+    'Wordpress',
+    'Bootstrap 3+',
+    'Git',
+    'React.JS',
+    'Gatsby.JS'
+]
+
+export default techs
