ed080978812047ae7806ca4cb36d314fc2d0a909
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-10 01:29:09 -0300

    Adicionado o conteudo de experiência

---
+++ src/components/Experience/content.js
@@ -0,0 +1,22 @@
+const jobs = [
+    {
+        date: 'Out 2018 - Presente',
+        company: 'Melhor Comunicação',
+        role: 'Estagiário Front-End',
+        description: 'Na Melhor Comunicação eu trabalho no desenvolvimento de sites e sistemas e-commerce com Wordpress. Utilizei Javascript para a criação de algumas funções necessárias para os sites e CSS3 para a estilização. Nesses meses que estou na empresa, mostrei aos desenvolvedores como utilizar o controle de versão com Git e utilizamos o bitbucket para manter os repositórios dos nossas aplicações.'
+    },
+    {
+        date: 'Ago - Set 2018',
+        company: 'Recife Sites',
+        role: 'Estagiário Front-End',
+        description: 'Na Recife Sites, eu aprendi a utilizar o pré-processador SASS e como utilizar determinados plugins jQuery que eram "padrões" no desenvolvimento dos sites. Nós utilizamos a versão 3.3.7 do Bootstrap para manter a responsividade e o Bitbucket para manter os repositórios dos sites. Com o Photoshop, nós visualizamos os layouts feitos pelo UI/UX Designer da empresa para realizar a criação da interface do site.'
+    },
+    {
+        date: 'Mai - Ago 2018',
+        company: 'Accelera Vendas',
+        role: 'Estagiário',
+        description: 'Na Accelera Vendas eu trabalhei com o CMS Wordpress para a criação e manutenção de sites. Na empresa eu solicitava imagens e textos às devidas pessoas, entrava em contato com o cliente para captar informações para o site e tirava dúvidas. Enquanto estive lá treinei 2 colaboradores, mostrando como utilizar o Wordpress e fazer determinadas criações e alterações dentro do site, e na minha saída, treinei o novo estagiário contratado.'
+    }
+]
+
+export default jobs
