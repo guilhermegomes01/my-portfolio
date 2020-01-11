6ed89fa83e59dbec0c3c83b508ada66997902eac
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-01-11 01:05:48 -0300

    Adicionado descrição dos sites

--- src/components/Portfolio/content.js
+++ src/components/Portfolio/content.js
@@ -2,26 +2,36 @@ const sites = [
     {
         title: "Veneza Water Park",
         type: "E-Commerce",
+        url: "https://venezawaterpark.com.br/",
+        description: "O Veneza é um parque aquático localizado no litoral norte de Pernambuco. É um e-commerce onde é possível fazer a compra de ingressos para ter acesso ao parque.",
         name: "veneza"
     }, 
     {
         title: "CCA Advogados",
         type: "Institucional",
+        url: "https://ccaadvogados.com.br/",
+        description: "CCA é um escritório de advocacia com unidades Recife, São Paulo e Brasília. Com especialistas nas áreas Civil e Empresarial, Administrativo, Tributário e Trabalhista.",
         name: "cca"
     },
     {
         title: "Melhor Comunicação",
         type: "Institucional",
+        url: "https://melhorcom.com.br/",
+        description: "A Melhor Comunicação é uma Agência que trabalha com grandes clientes e tem diversos caminhos e estratégias para aumentar as suas vendas.",
         name: "melhorcom"
     },
     {
         title: "Novo Nascer",
         type: "Institucional",
+        url: "https://www.novonascer.com.br/",
+        description: "Novo Nascer é uma clínica hospitalar com uma estrutura adequada para pessoas que precisam de tratamento em Saúde Mental, Dependência Química, entre outros.",
         name: "novoNascer"
     },
     {
         title: "Agência Cósmica",
         type: "Institucional",
+        url: "https://www.agenciacosmica.com.br/",
+        description: "Cósmica é uma Agência que trabalha com Marketing Digital em Recife e oferece serviços como Gestão de Mídias Sociais, Gestão de Anúncios, Criação de Sites, entre outros.",
         name: 'agenciaCosmica'
     }
 ]
