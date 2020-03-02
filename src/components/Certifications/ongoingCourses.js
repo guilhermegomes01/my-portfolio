2994cf9cb3413e0afb47cba7e239a86f2859598e
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-03-01 22:05:51 -0300

    Adicionado novo curso

--- src/components/Certifications/ongoingCourses.js
+++ src/components/Certifications/ongoingCourses.js
@@ -1,5 +1,9 @@
 const ongoingCourses = [
     {
+        name: 'Java 13 Completo: Do Zero ao Profissional',
+        plataform: 'Udemy',
+    },
+    {
         name: 'Curso React.JS Ninja - React Completo',
         plataform: 'Udemy',
     },
