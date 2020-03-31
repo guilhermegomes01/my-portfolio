91bd3627bf12d6f3dffec885d0b628bf38679b5f
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-03-31 00:31:21 -0300

    Criação do componente Covid19

---
+++ src/components/CoronaVirus/index.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react"
+import Chart from "react-apexcharts"
+import api from "../../services/covid/api"
+import moment from "moment"
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts"
+
+import { TemplateWrapper, TemplateFeatures, TemplateDiv } from "./styled"
+import TemplateCases from "./components/TemplateCases"
+
+const CoronaVirus = () => {
+  const [status, setStatus] = useState({})
+  const [statusPE, setStatusPE] = useState({})
+  // const [statusDate, setStatusDate] = useState('')
+  // const [statusDate2, setStatusDate2] = useState('')
+  // const [statusDate3, setStatusDate3] = useState('')
+  const [loading, setLoading] = useState(true)
+  // const data = [
+  //   { name: "27/03", Casos: statusDate },
+  //   { name: "28/03", Casos: statusDate2, pv: 2200, amt: 2200 },
+  //   { name: "29/03", Casos: statusDate3, pv: 2400, amt: 2400 },
+  // ]
+
+  useEffect(() => {
+    async function loadStatus() {
+      const response = await api.get(`/brazil`)
+      setStatus(response.data.data)
+    }
+
+    async function loadStatusPE() {
+      const response = await api.get(`/brazil/uf/pe`)
+      setStatusPE(response.data)
+      setLoading(false)
+    }
+
+    // async function loadStatusDate() {
+    //   const response = await api.get(`/brazil/20200327`)
+    //   const secondResponse = await api.get(`/brazil/20200328`)
+    //   const thirdResponse = await api.get(`/brazil/20200329`)
+    //   response.data.data.map(res => res.uf === 'PE' && setStatusDate(res.cases));
+    //   secondResponse.data.data.map(res => res.uf === 'PE' && setStatusDate2(res.cases));
+    //   thirdResponse.data.data.map(res => res.uf === 'PE' && setStatusDate3(res.cases));
+
+    // }
+    loadStatus()
+    // loadStatusDate()
+    loadStatusPE()
+  }, [])
+
+  const { uf, state, cases, deaths, suspects, refuses, datetime } = statusPE
+  // const { country, cases, confirmed, deaths, recovered, updated_at } = status;
+  console.log()
+  return (
+    <>
+      <TemplateWrapper>
+        {loading ? (
+          <TemplateDiv>
+            <div></div>
+            <div></div>
+          </TemplateDiv>
+        ) : (
+          <>
+            <TemplateCases
+              isState
+              title="Casos em Pernambuco"
+              confirmed={cases}
+              deaths={deaths}
+              suspects={suspects}
+              refuses={refuses}
+              datetime={moment(datetime).format("D/MM/YYYY HH:mm")}
+            />
+            <TemplateCases
+              title="Casos no Brasil"
+              confirmed={status.cases}
+              deaths={status.deaths}
+              suspects={status.recovered}
+              refuses={status.confirmed}
+              datetime={moment(status.updated_at).format("D/MM/YYYY HH:mm")}
+            />
+          </>
+        )}
+
+        {/* <LineChart width={400} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Line type="monotone" dataKey="Casos" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" trokeDasharray="5 5"/>
+        <YAxis />
+        <Tooltip />
+      </LineChart> */}
+      </TemplateWrapper>
+      <TemplateFeatures>
+        <h3>Mais funcionalidades em Desenvolvimento...</h3>
+        <p>
+          Em breve: Dados das cidades de Pernambuco que contém ao menos 1 caso
+          confirmado.
+        </p>
+      </TemplateFeatures>
+    </>
+  )
+}
+
+export default CoronaVirus
---
+++ src/components/CoronaVirus/styled.js
@@ -0,0 +1,72 @@
+import styled from "styled-components"
+import media from "styled-media-query"
+
+export const TemplateWrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+  ${media.lessThan("991.98px")`
+        width: 17rem;
+        margin: 30px auto 0px;
+        flex-direction: column;
+    `}
+`
+
+export const TemplateFeatures = styled.section`
+  flex-direction: column;
+  p,
+  h3 {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
+  ${media.lessThan("991.98px")`
+        width: 17rem;
+        margin: 30px auto 90px;
+    `}
+
+  ${media.greaterThan("992px")`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 40%;
+    `}
+`
+
+export const TemplateDiv = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+  margin: auto;
+
+  div {
+    position: absolute;
+    border: 4px solid #333;
+    opacity: 1;
+    border-radius: 50%;
+    animation: loader 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+
+    &:nth-child(2) {
+      animation-delay: -0.5s;
+    }
+  }
+
+  @keyframes loader {
+    0% {
+      top: 28px;
+      left: 28px;
+      width: 0;
+      height: 0;
+      opacity: 1;
+    }
+
+    100% {
+      top: -1px;
+      left: -1px;
+      width: 58px;
+      height: 58px;
+      opacity: 0;
+    }
+  }
+`
