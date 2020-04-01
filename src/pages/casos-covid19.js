178b8cf8daa591502c1470ce125c579e15d8f5f1
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-03-31 21:13:14 -0300

    Organizacao do codigo

--- src/pages/casos-covid19.js
+++ src/pages/casos-covid19.js
@@ -2,16 +2,20 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import Header from '../components/Header'
-import CoronaVirus from '../components/CoronaVirus'
+import Header from "../components/Header"
+import CoronaVirus from "../components/CoronaVirus"
 
 const CasosCovid19 = () => (
   <>
-  <Header />
-  <Layout>
-    <SEO title="Casos Covid19" image='/assets/img/guilherme-icon.png' description="Informações sobre os casos de Covid19 em Pernambuco e suas cidades, e também, no Brasil."/>
-    <CoronaVirus />
-  </Layout>
+    <Header />
+    <Layout>
+      <SEO
+        title="Casos Covid19"
+        image="/assets/img/guilherme-icon.png"
+        description="Informações sobre os casos de Covid19 em Pernambuco e suas cidades, e também, no Brasil."
+      />
+      <CoronaVirus />
+    </Layout>
   </>
 )
 
