11b896ddd8f39f48cf01ecfa9b2f303693ed6ef4
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-04-08 21:57:20 -0300

    Adicionado page de Informacoes importantes

---
+++ src/pages/casos-covid19/informacoes-importantes/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Layout from "../../../components/Layout"
+import SEO from "../../../components/seo"
+import Header from "../../../components/Header"
+import Informations from "../../../components/Informations"
+
+const ImportantInformations = () => (
+  <>
+    <Header />
+    <Layout>
+      <SEO
+        title="Casos Covid19"
+        image="/assets/img/guilherme-icon.png"
+        description="Informações sobre os casos de Covid19 no Brasil, nos seus estados e respectivas cidades."
+      />
+      <Informations />
+    </Layout>
+  </>
+)
+
+export default ImportantInformations
