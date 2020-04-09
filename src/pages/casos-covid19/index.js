7403131358b25cfe9798c04e783cf02ae7f862d0
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-04-08 21:57:32 -0300

    Alterado caminho da page covid

--- src/pages/casos-covid19/index.js
+++ src/pages/casos-covid19/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-import Layout from "../components/Layout"
-import SEO from "../components/seo"
-import Header from "../components/Header"
-import CoronaVirus from "../components/CoronaVirus"
+import Layout from "../../components/Layout"
+import SEO from "../../components/seo"
+import Header from "../../components/Header"
+import CoronaVirus from "../../components/CoronaVirus"
 
 const CasosCovid19 = () => (
   <>
