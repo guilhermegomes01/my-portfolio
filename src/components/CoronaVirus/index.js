dbe5528714ea1e2ee4f2f2851d287a9121e6ae32
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-04-09 02:15:32 -0300

    Adicionado botões de compartilhamento

--- src/components/CoronaVirus/index.js
+++ src/components/CoronaVirus/index.js
@@ -4,15 +4,36 @@ import React, { useState, useEffect } from "react"
 import { api, apiSecretaria } from "../../services/covid/api"
 import moment from "moment"
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts"
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "react-share"
 
 // Icons
 import Globe from "../../images/svg-icons/globe.svg"
+import { ArrowRight } from "@styled-icons/feather/ArrowRight"
+import {
+  FacebookIcon,
+  LinkedinIcon,
+  TelegramIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share"
 
 // Content
 import links from "../MenuLinks/content"
 
 // Styles
-import { TemplateWrapper, TemplateFeatures, TemplateDiv } from "./styled"
+import {
+  TemplateWrapper,
+  TemplateFeatures,
+  TemplateDiv,
+  Button,
+  Span,
+} from "./styled"
 
 // Internal Components
 import PageTitle from "../PageTitle"
@@ -20,7 +41,9 @@ import SelectState from "./components/SelectState"
 import TemplateCountryAndState from "./components/TemplateCountryAndState"
 import TemplateCities from "./components/TemplateCities"
 
-const CoronaVirus = () => {
+const CoronaVirus = ({ title, description }) => {
+  const url = "https://guilhermegomes.dev/casos-covid19/"
+
   const [actualState, setActualState] = useState("PE")
   const [status, setStatus] = useState({})
   const [statusPE, setStatusPE] = useState({})
@@ -58,7 +81,6 @@ const CoronaVirus = () => {
     setActualState(event.target.value)
   }
 
-  console.log(loading)
   const { uf, state, cases, deaths, suspects, refuses, datetime } = statusPE
 
   return (
@@ -67,13 +89,51 @@ const CoronaVirus = () => {
         <Globe />
         <h2>{links[4].label}</h2>
       </PageTitle>
-      <TemplateWrapper style={{ marginTop: 40 }}>
-        <p style={{ textAlign: "center", lineHeight: "23px" }}>
-          Obs.: Os casos por município de um estado pode divergir do total no
-          estado, pois nem sempre as Secretarias divulgam todos os municípios em
-          que aconteceram casos.
-        </p>
+
+      <TemplateWrapper className="social-links" style={{ marginTop: 40 }}>
+        <p className="share-button text-share">Compartilhe em: </p>
+        <WhatsappShareButton
+          className="share-button"
+          url={url}
+          title={title}
+          separator=": "
+        >
+          <WhatsappIcon size={35} borderRadius={3} />
+        </WhatsappShareButton>
+        <LinkedinShareButton
+          className="share-button"
+          url={url}
+          title={`${title} no Brasil e seus estados`}
+          source="Guilherme Gomes"
+          summary={description}
+        >
+          <LinkedinIcon size={35} borderRadius={3} />
+        </LinkedinShareButton>
+        <FacebookShareButton
+          className="share-button"
+          url={url}
+          quote={description}
+        >
+          <FacebookIcon size={35} borderRadius={3} />
+        </FacebookShareButton>
+        <TwitterShareButton className="share-button" url={url} title={title}>
+          <TwitterIcon size={35} borderRadius={3} />
+        </TwitterShareButton>
       </TemplateWrapper>
+
+      <TemplateWrapper>
+        <Button
+          cover
+          direction="left"
+          bg="#242424"
+          duration={0.6}
+          to="/casos-covid19/informacoes-importantes"
+        >
+          <Span>Informações Importantes</Span>
+          <ArrowRight />
+        </Button>
+      </TemplateWrapper>
+
       <TemplateWrapper className="first">
         {loading ? (
           <TemplateDiv>
@@ -86,7 +146,7 @@ const CoronaVirus = () => {
             confirmed={status.confirmed}
             deaths={status.deaths}
             suspects={status.recovered}
-            datetime={moment(status.updated_at).format("D/MM/YYYY HH:mm")}
+            datetime={moment(status.updated_at).format("DD/MM/YYYY HH:mm")}
           />
         )}
       </TemplateWrapper>
--- src/components/CoronaVirus/styled.js
+++ src/components/CoronaVirus/styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import media from "styled-media-query"
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 export const TemplateWrapper = styled.section`
   display: flex;
@@ -18,11 +19,31 @@ export const TemplateWrapper = styled.section`
     margin-top: 30px;
   }
 
+  &.social-links {
+    justify-content: flex-start;
+    align-items: center;
+
+    .share-button {
+      margin-right: 10px;
+    }
+  }
+
+
+
   ${media.lessThan("991.98px")`
-        width: 17rem;
-        margin: 30px auto 0px;
-        flex-direction: column;
-    `}
+      width: 17rem;
+      margin: 30px auto 0px;
+      flex-direction: column;
+
+      &.social-links {
+        flex-direction: row;
+        
+        .text-share {
+          flex-basis: 100%;
+          margin-bottom: 15px;
+        }
+      }
+  `}
 `
 
 export const TemplateFeatures = styled.section`
@@ -83,4 +104,43 @@ export const TemplateDiv = styled.div`
       opacity: 0;
     }
   }
+`
+
+export const Button = styled(AniLink)`
+  margin-top: 30px;
+  font-family: "Quicksand";
+  display: inline-flex;
+  align-items: center;
+  color: #A5A5A5;
+  padding: 5px 15px;
+  border: 1px solid;
+  border-radius: 3px;
+  background: transparent;
+  text-decoration: none;
+  transition: .5s;
+
+  &:hover {
+      background: #a5a5a5;
+      color: #242424;
+  }
+
+  svg {
+      width: 20px;
+      height: auto;
+  }
+
+  ${media.lessThan("991.98px")`
+      margin-top: 0px;
+  `}
+`
+
+export const Span = styled.span`
+  font-size: 16px;
+  padding-right: 10px;
+  text-transform: uppercase;
+
+  ${media.lessThan("991.98px")`
+      font-size: 15px;
+      padding-right: 5px;
+  `}
 `
