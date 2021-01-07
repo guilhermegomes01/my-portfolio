0c9e66b1f0f3ffcc8c909521a7f57ec99c68cec4
Guilherme Gomes <guiga.gomes10@gmail.com>
2021-01-07 06:26:21 -0300

    Atualizado porcentagem dos cursos em andamento

--- src/components/Certifications/ongoingCourses.js
+++ src/components/Certifications/ongoingCourses.js
@@ -1,18 +1,18 @@
 const ongoingCourses = [
     {
+        name: 'React Avançado: Crie aplicações com NextJS, GraphQL e mais.',
+        plataform: 'Udemy',
+        percentage: '35%'
+    },
+    {
         name: 'React Native - The Pratical Guide 2020',
         plataform: 'Udemy',
         percentage: '35%'
     },
     {
-        name: 'React Avançado: Crie aplicações com NextJS, GraphQL e mais.',
-        plataform: 'Udemy',
-        percentage: '30%'
-    },
-    {
         name: 'React e TypeScript: Desenvolvendo uma Dashboard',
         plataform: 'Udemy',
-        percentage: '15%'
+        percentage: '25%'
     },
 ]
 
